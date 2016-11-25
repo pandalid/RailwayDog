cedce07deb28db780bef73d1bd5a2973de34f039
Guogle <guoshengzhao2008@gmail.com>
2016-11-25 22:49:52 +0800

    接收消息并打印

--- webot.py
+++ webot.py
@@ -2,8 +2,8 @@ import itchat
 from itchat.content import *
 
 @itchat.msg_register(TEXT)
-def replay(msg):
-    print(msg['Text'])
+def text_reply(msg):
+    itchat.send('%s' % msg['Text'], msg['FromUserName'])
 
 itchat.auto_login(hotReload=True)
 itchat.run()
